c388a8b439c88f40240ca7601395424bb6ab5e4d
M.Anees <manees7865@gmail.com>
2024-12-25 02:01:01 +0500

    "banner done"

--- src/components/Blog/Blog.tsx
+++ src/components/Blog/Blog.tsx
@@ -1,20 +1,13 @@
 import React from 'react'
 import Image from 'next/image'
+import Banner from '../Banner/Banner'
 
 const Blog = () => {
   return (
     <>
     <section className='min-h-full w-full flex flex-col   items-center'>
             {/* banner */}
-       <div className='h-full md:h-[316px] w-[90%] md:w-[1440px] flex flex-col  items-center justify-center'>
-       <div className=' md:h-[316px] relative w-[90%] md:w-[1440px] '>
-      <Image src={'/images/shopbenner.png'} alt='hero' width={1440} height={316} />
-      <div className='w-[150px] md:w-[124px] absolute translate-x-[-50%] translate-y-[-50%] top-[50%] left-[50%] h-[90%] flex flex-col justify-center items-center md:h-[84px] '>
-        <h1 className='font-medium text-4xl text-black'>Blog</h1>
-        <p className='font-normal text-[16px] text-mygray'>home&gt;Blog</p>
-        </div>
-      </div>
-      </div>
+      <Banner title='Blog' breadcrumb='home >blog' />
       {/* blog */}
       <div className= 'h-full md:h-[2710px] w-[90%] flex flex-col  items-center justify-between space-y-4 py-12 md:w-[1440px]  '> 
         <div className='h-[3800px] md:h-[2610px] w-[90%] md:w-[1210px] flex flex-col md:flex-row justify-between '>
